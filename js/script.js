3c09553b18eed8e7cfca140345db61bc13e0179c
teamie <timmieprince@gmail.com>
2022-03-13 17:10:31 +0800

    minor mods

--- js/script.js
+++ js/script.js
@@ -5,34 +5,23 @@ const id = get('#id');
 const content = get('.advice-content');
 const URL = 'https://api.adviceslip.com/advice';
 
-window.addEventListener('DOMContentLoaded', async function () {
-  const randomID = Math.ceil(Math.random() * 100);
-  const randomURL = 'https://api.adviceslip.com/advice/';
-  try {
-    const response = await fetch(`${randomURL}${randomID}`);
-    const data = await response.json();
-    displayAdvice(data);
-  } catch (error) {
-    console.log(error);
-  }
-});
+btn.addEventListener('click', displayAdvice);
+window.addEventListener('DOMContentLoaded', displayAdvice);
 
-btn.addEventListener('click', async () => {
+const fetchAdvice = async () => {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    displayAdvice(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
-});
+};
 
 // Display Advice in DOM
-function displayAdvice(data) {
+async function displayAdvice() {
+  const data = await fetchAdvice();
   const { id: adviceId, advice } = data.slip;
   content.innerHTML = `<p>"${advice}"</p>`;
   id.innerHTML = adviceId;
-  setTimeout(function () {
-    btn.classList.remove('animate');
-  }, 3000);
 }
